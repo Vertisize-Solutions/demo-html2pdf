4ec8518245149ce8dabc055e9d0d2453e5f6fe37
Kolappan N <>
2018-11-08 15:50:49 +0530

    Added Logger

---
+++ src/Core.Lib/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Core.Lib
+{
+    public class Logger
+    {
+        private nk.logger.csv.Logger csvLogger;
+        private string dateFormat = "yyyy-MM-dd HH:mm:ss.fff";
+        private string fileName = "ErrorLog";
+
+        public Logger()
+        {
+            csvLogger = new nk.logger.csv.Logger(dateFormat, fileName);
+        }
+
+        public void Error(Exception ex)
+        {
+            csvLogger.Error(ex);
+        }
+        public void Error(string text)
+        {
+            csvLogger.Error(text);
+        }
+
+        public void Info(string text)
+        {
+            csvLogger.Info(text);
+        }
+
+    }
+}
