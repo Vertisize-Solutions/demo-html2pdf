36557b3a02ed7e64da90ababde30a495fbfce231
Kolappan N <>
2018-11-09 15:48:14 +0530

    Added missing files

---
+++ src/Business.Lib/iTextLib.cs
@@ -0,0 +1,28 @@
+﻿using Core.Lib;
+using System;
+
+namespace Business.Lib
+{
+    public class iTextLib
+    {
+
+        private Logger logger;
+
+        public iTextLib()
+        {
+            logger = new Logger();
+        }
+
+        public void html2pdf(string html, string css)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+        }
+    }
+}
